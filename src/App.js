c7ad45ea632a8bac935412caff7fc57c8186e230
Pooja-Chelani <poojachelani@gmail.com>
2021-04-01 10:22:19 +0530

    my first commit reactwebsite

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.js";
 import Home from "./Home";
 import About from "./About";
 import Services from "./Services";
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+
+import React from "react";
 import ReactDom from "react-dom";
 import {BrowserRouter} from "react-router-dom"; 
 import App from "./App";
-import "./Home.css";
+import "./Home.scss";
 ReactDom.render(
   <>
 <BrowserRouter>
