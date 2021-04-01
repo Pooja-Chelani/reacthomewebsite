f1417ae20e15b35579c4d2fc9d1dd0abab4f7dc7
Pooja-Chelani <poojachelani@gmail.com>
2021-04-01 10:50:46 +0530

    my first commit reactwebsite

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import React from "react";
 import ReactDom from "react-dom";
 import {BrowserRouter} from "react-router-dom"; 
 import App from "./App";
-import "./Home.scss";
+import "./Home.css";
 ReactDom.render(
   <>
 <BrowserRouter>
